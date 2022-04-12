ad6e8809fb87bea8c948c81fd59604f1270ee684
bezawitGT <bezagetachew.bg@gmail.com>
2022-04-12 12:01:02 -0700

    hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - prints the string "Holberton" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	
 	printf("_putchar\n");
 	return (0);
 }
