16a0cdb80550f8e2693609f2ccb0874313cb9f83
bezawitGT <bezagetachew.bg@gmail.com>
2022-04-13 10:02:05 +0300

    hi

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,31 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * _abs - checks for alphabetic character.
+ * @n: accept input  parameter n of type int
+ * Return: 1 and prints + if n is greater than zero
+ */
 int _abs(int n)
 {
-	if(n>0){
-		return(n);
-	}else if(n==0)
+	if (n > 0){
+		return (n);
+	}else if (n == 0)
 	{
-		return(0);
-	}else{
-		return((-1*n));
+		return (0);
 	}
+	return ((-1*n));
 	}
-int main(void)
-{
-    int r;
-
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
-}
