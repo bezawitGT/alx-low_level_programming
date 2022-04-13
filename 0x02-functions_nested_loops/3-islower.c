9a6a02d82c8e8cb071717ca90c6358cc8cad87e4
bezawitGT <bezagetachew.bg@gmail.com>
2022-04-13 09:42:51 +0300

    hello

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * checks for lowercase character.
- * Returns 1 if c is lowercase.
- * Returns 0 otherwise.
+ * islower - checks for lowercase character.
+ * Returns: 1 if c is lowercase.
+ * Returns: 0 otherwise.
  */
 int _islower(int c)
 {
-	if (c>=97){
+	if (c >= 97)
+	{
 		return(1);
-	}else{
+	}else
+	{
 		return(0);
 	}
 }
