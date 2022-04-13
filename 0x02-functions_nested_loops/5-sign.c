7f9683acd65b063a80bb36c787aab4cad7eae17e
bezawitGT <bezagetachew.bg@gmail.com>
2022-04-13 09:47:05 +0300

    hello

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,46 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-/* more headers goes there */
-
+/* print_sign
+*
 /* betty style doc for function main goes there */
 int print_sign(int n)
 {
-	if(n>0){
-		putchar('+');
-		return(1);
-	}else if(n==0)
+	if(n > 0)
 	{
-		putchar('0');
-		return(0);
-	}else{
-		putchar('-');
-		return(-1);
+		putchar('+');
+		return (1);
+	}else if(n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}else
+	{
+		_putchar('-');
+		return (-1);
 	}
 	}
-int main(void)
-{
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
-}
