82bf9f09d63c499e8389b2b72ef1b366b0b3b947
bezawitGT <bezagetachew.bg@gmail.com>
2022-04-14 13:38:15 +0300

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _isupper - checks for uppercase character.
+ * @c: accept input  parameter c of type int
+ * Return: 1 if c is upper,0 otherwise.
+ */
+int _isupper(int c)
+{
+	if ((c =< 90) && (c => 65))
+	{
+		return (1);
+	}
+	return (0);
+}
