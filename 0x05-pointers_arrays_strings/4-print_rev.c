5bcde5caebc22079a2bcce11ff2dc332a1a7ad13
bezawitGT <bezagetachew.bg@gmail.com>
2022-04-18 15:08:57 +0300

    Add files via upload

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_rev -  function that swaps the values of two integers.
+ * @s: accept input  parameter c of type int
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int left, len, i;
+
+	len = 0;
+	left = 0;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		len = i;
+	}
+	for (; len >= left; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
