7d8f1895cccf75c15fc355c401ab5d7c0c3b1d75
bezawitGT <bezagetachew.bg@gmail.com>
2022-04-27 15:10:16 +0300

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,3 +10,5 @@ int is_square_root(int i, int j);
 int is_prime(int i, int j);
 int is_palindrome(char *s);
 int check_palindrome(char *s, int len, int i);
+int wildcmp(char *s1, char *s2);
+char *wildcard(char *s3, char *s4);
