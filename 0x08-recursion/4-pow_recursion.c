92136e9602f91191a9bbbfbc395347060e5d5406
bezawitGT <bezagetachew.bg@gmail.com>
2022-04-27 13:24:55 +0300

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return string len
+ * @x: input int
+ * @y: input int
+ * Description: returns the factorial of a numb
+ * Return: returns int len
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x , y - 1));
+}
