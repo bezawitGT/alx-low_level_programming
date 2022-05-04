0483fefe208f7da77a6903d3a3bb1ebcebe3fec1
bezawitGT <bezagetachew.bg@gmail.com>
2022-05-04 13:57:18 +0300

    argv,argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,11 @@
 
 int main(int argc, char **argv)
 {
-	int mult;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	mult= atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", mult);
+	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 	return (0);
 }
