abb14d5241dc444a4c13ed6dd2fdd0a78015dd58
bezawitGT <bezagetachew.bg@gmail.com>
2022-05-12 12:01:17 +0300

    fuc pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,9 @@
-int _putchar(int c);
+#ifndef _function_pointers_
+#define _function_pointers_
+#include <stdio.h>
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+#endif
